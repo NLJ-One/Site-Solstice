795c166118c8f987fd193d1cd4231294e01e8d9c
Jean-Louis Noah <noahbusinessproject.jl@gmail.com>
2025-06-06 23:51:13 +0200

    Add files via upload

--- www/html/1-index.php
+++ www/html/1-index.php
@@ -32,38 +32,31 @@
     </nav>
   </header>
   <main>
-      <div id="PI1">
-        <img id="imgac" src="images/b orange.png" alt="bières" />
-        <div id="PI1_1">
+    <div id="PI1">
+      <img id="imgac" src="images/b orange.png" alt="bières" />
+      <div id="PI1_1">
         <h1> Notre nouvelle bière Orangée</h1>
         <h2>Une bière avec un goût unique qui vous rappeleras l'été à chaque gorgée</h2>
         <h3 class="bouton"><a href="nosbieres.php">Acheter maintenant</h3></a>
       </div>
-      </div>
+    </div>
     </div>
 
     <div id="PI2">
-      <a href="nosbieres.php"><h1>Découvrez nos bières</h1></a>
+      <a href="nosbieres.php">
+        <h1>Découvrez nos bières</h1>
+      </a>
       <img src="images/mockup-pack-orcopa.webp" alt="Bière-présentation" />
     </div>
 
     <div id="PI3">
-      <div class="contact">
-        <h1>Contactez-nous</h1>
-        <form action="#" method="post">
-          <label for="name">Nom</label>
-          <input type="text" id="name" name="name" required>
-
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" required>
-
-          <label for="message">Message</label>
-          <textarea id="message" name="message" rows="5" required></textarea>
-
-          <button type="submit">Envoyer</button>
-        </form>
+      <h1>Pub Solstice</h1>
+      <div class="video-container">
+        <video controls>
+          <source src="images/pub_groupek_sae_but1_mmi.mp4" type="video/mp4">
+        </video>
       </div>
-      <img id="bf" src="images/B pomme.png" />
+    </div>
   </main>
 
   <footer class="footer">
